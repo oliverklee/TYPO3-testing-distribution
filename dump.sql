36a2d3cca4b929bd873d83762e63f68b6e7a0b33
Oliver Klee <typo3-coding@oliverklee.de>
2023-04-10 16:10:34 +0200

    [TASK] Update the DB dump

--- dump.sql
+++ dump.sql
@@ -1,10 +1,10 @@
 
 -- Dump of TYPO3 Connection "Default"
--- MariaDB dump 10.19  Distrib 10.5.15-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.19  Distrib 10.5.18-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: db    Database: db
 -- ------------------------------------------------------
--- Server version	10.6.11-MariaDB-1:10.6.11+maria~ubu2004-log
+-- Server version	10.6.12-MariaDB-1:10.6.12+maria~ubu2004-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -2333,4 +2333,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-04 18:22:49
+-- Dump completed on 2023-04-10 16:10:24
