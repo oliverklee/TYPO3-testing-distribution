ed3a71de0ac99b8016d99eeed3bac148e4d6f892
Oliver Klee <typo3-coding@oliverklee.de>
2023-09-20 18:48:07 +0200

    [TASK] Integrate the site package extension
    This will make maintenance of the site package less work.

---
+++ src/extensions/typo3-devsite/ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '@import "EXT:typo3_devsite/Configuration/TsConfig/Page/"'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+    '@import "EXT:typo3_devsite/Configuration/TsConfig/User/"'
+);
