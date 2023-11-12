aa6e798724c793d71c65e0133227b1035c8b9453
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-12 16:31:17 +0100

    [CLEANUP] Autoformat the code (#154)

--- src/extensions/site-dev/Configuration/TCA/Overrides/sys_template.php
+++ src/extensions/site-dev/Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,9 @@
 <?php
+
 defined('TYPO3') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-  'site_dev',
-  'Configuration/TypoScript',
-  'TYPO3 development site'
+    'site_dev',
+    'Configuration/TypoScript',
+    'TYPO3 development site'
 );
