a2de4935da433c4f3ff8f36e9cb1bc2f36c94155
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-12 18:29:36 +0100

    [FEATURE] Add unit tests
    Part of #146

---
+++ src/extensions/site-dev/Tests/Unit/UniverseTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\SiteDev\Tests\Unit;
+
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * @coversNothing
+ */
+final class UniverseTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function theTruthIsOutThere(): void
+    {
+        self::assertTrue(true);
+    }
+}
