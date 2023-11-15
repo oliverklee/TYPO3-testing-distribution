ec0c7d09c6bdb7adb5912f73c917a2d972947cc4
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-15 13:19:15 +0100

    add room plugin to new content element wizard

---
+++ src/extensions/slub-structured-content/Configuration/Icons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // use same identifier as used in TSconfig for icon
+    'structuredcontent_room' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:structured_content/Resources/Public/Icons/Extension.svg',
+    ],
+];
