0085f628382268395f14d639b70e52f2afde8635
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-15 14:21:53 +0100

    add typo311 compability for tsconfig import

--- src/extensions/slub-structured-content/ext_localconf.php
+++ src/extensions/slub-structured-content/ext_localconf.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use Slub\StructuredContent\Controller\RoomController;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die('Access denied.');
 
@@ -21,3 +24,11 @@ ExtensionUtility::configurePlugin(
     RoomController::class => 'search',
     ]
 );
+
+$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+if ($versionInformation->getMajorVersion() < 12) {
+    ExtensionManagementUtility::addPageTSConfig(
+        '@import "EXT:structured_content/Configuration/page.tsconfig"'
+    );
+}
