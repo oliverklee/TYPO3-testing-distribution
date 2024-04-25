c01cce8a6ed1254fe4af0da5cb99465e75d6af61
Oliver Klee <typo3-coding@oliverklee.de>
2024-04-25 19:40:33 +0200

    [FEATURE] Configure more logging (#299)

--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -1,4 +1,8 @@
 <?php
+
+use TYPO3\CMS\Core\Log\LogLevel;
+use TYPO3\CMS\Core\Log\Writer\FileWriter;
+
 return [
     'BE' => [
         'debug' => true,
@@ -99,17 +103,48 @@ return [
         'processor_path_lzw' => '/usr/bin/',
     ],
     'LOG' => [
+        'writerConfiguration' => [
+            LogLevel::DEBUG => [
+                FileWriter::class => [
+                    'disabled' => false,
+                    'logFileInfix' => 'debug',
+                ],
+            ],
+            LogLevel::INFO => [
+                FileWriter::class => [
+                    'disabled' => false,
+                    'logFileInfix' => 'info',
+                ],
+            ],
+            LogLevel::NOTICE => [
+                FileWriter::class => [
+                    'disabled' => false,
+                    'logFileInfix' => 'notice',
+                ],
+            ],
+            LogLevel::WARNING => [
+                FileWriter::class => [
+                    'disabled' => false,
+                    'logFileInfix' => 'warning',
+                ],
+            ],
+        ],
         'TYPO3' => [
+            'HtmlSanitizer' => [
+                'writerConfiguration' => [
+                    LogLevel::DEBUG => [
+                        FileWriter::class => [
+                            'disabled' => false,
+                            'logFileInfix' => 'html',
+                        ],
+                    ],
+                ],
+            ],
             'CMS' => [
                 'deprecations' => [
                     'writerConfiguration' => [
-                        5 => [
-                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                                'disabled' => false,
-                            ],
-                        ],
-                        'notice' => [
-                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                        LogLevel::NOTICE => [
+                            FileWriter::class => [
                                 'disabled' => false,
                             ],
                         ],
