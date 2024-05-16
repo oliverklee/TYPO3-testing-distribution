158b143f7ccec73496b854e508e1fec5db3e1cad
Oliver Klee <typo3-coding@oliverklee.de>
2024-05-16 22:43:20 +0200

    [TASK]  Use the parallel runner for PHP-CS-Fixer
    Fixes #322

--- config/system/additional.php
+++ config/system/additional.php
@@ -5,7 +5,6 @@
  * ddev manages this file and may delete or overwrite the file unless this comment is removed.
  * It is recommended that you leave this file alone.
  */
-
 if (getenv('IS_DDEV_PROJECT') == 'true') {
     $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
         $GLOBALS['TYPO3_CONF_VARS'],
