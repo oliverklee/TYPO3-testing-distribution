20f0e489b504fb11305eac85e0dd70725310e43b
Oliver Klee <typo3-coding@oliverklee.de>
2024-07-22 13:07:44 +0200

    [BUGFIX] Stop style-checking autogenerated configuration files

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
-$config->getFinder()->in(__DIR__ . '/config')->in(__DIR__ . '/src/extensions/site-dev');
+$config->getFinder()->in(__DIR__ . '/src/extensions/site-dev');
 return $config;
