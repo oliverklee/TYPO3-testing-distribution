e016099cccca05de2a691c882af5786560fba5bb
Oliver Klee <typo3-coding@oliverklee.de>
2026-02-10 19:02:18 +0100

    [TASK] Ensure the BE can only be accessed via HTTPS
    https://docs.typo3.org/permalink/t3coreapi:security-global-typo3-options-lockssl

--- config/system/settings.php
+++ config/system/settings.php
@@ -9,6 +9,7 @@ return [
     'BE' => [
         'debug' => true,
         'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$LllPa2ZNY29LU2RRcGpUdQ$SgfBdtrn3pBh7s7NeGipTaTd0K3F67iAJCmSQB9RbI8',
+        'lockSSL' => true,
         'passwordHashing' => [
             'className' => Argon2idPasswordHash::class,
             'options' => [],
